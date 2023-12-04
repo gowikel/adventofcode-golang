33441b7aa99ebf4ae7ee631a70dd4c5c5c2a1f7c
Pedro Piquero <gowikel@gmail.com>
2023-12-04 19:32:46 +0000

    Add part 2 (failing at the moment)

--- 2023/day1/main.go
+++ 2023/day1/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	_ "embed"
 	"fmt"
 	"regexp"
@@ -8,34 +10,117 @@ import (
 	"strings"
 )
 
+var NUMBER = regexp.MustCompile(`\d`)
+var STARTS_WITH_WORD_NUMBER = regexp.MustCompile(`^(one|two|three|four|five|six|seven|eight|nine|zero)`)
 var NOT_A_NUMBER = regexp.MustCompile(`\D`)
 
 //go:embed data/2023_1.txt
 var data string
 
+// ParseInput takes a string and returns a slice of ints, ignoring
+// any non-numeric characters.
+//
+// A number will be added to the slice per line. The number
+// will be the first and last digit of the line.
+//
+// If the line has only one digit, it will be repeated twice.
 func ParseInput(input string) []int {
 	lines := strings.Split(input, "\n")
 	result := make([]int, 0, len(lines))
 
 	for _, line := range lines {
 		parsedLine := NOT_A_NUMBER.ReplaceAllString(line, "")
+
+		if len(parsedLine) == 0 {
+			continue
+		}
+
 		result = append(result, ParseNumber(parsedLine))
 	}
 
 	return result
 }
 
+// tokenizer is a split function for a Scanner that will recognize
+// numbers and words that represent numbers.
+//
+// It returns each token as a byte slice and converts words to numbers
+// before returning them.
+//
+// If the token is not a number or a word, it will return the token as it is.
+//
+// This function does not return an error.
+func tokenizer(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	} else if NUMBER.Match(data[:1]) {
+		return 1, data[:1], nil
+	} else if STARTS_WITH_WORD_NUMBER.Match(data) {
+		if bytes.HasPrefix(data, []byte("one")) {
+			return 3, []byte("1"), nil
+		} else if bytes.HasPrefix(data, []byte("two")) {
+			return 3, []byte("2"), nil
+		} else if bytes.HasPrefix(data, []byte("three")) {
+			return 5, []byte("3"), nil
+		} else if bytes.HasPrefix(data, []byte("four")) {
+			return 4, []byte("4"), nil
+		} else if bytes.HasPrefix(data, []byte("five")) {
+			return 4, []byte("5"), nil
+		} else if bytes.HasPrefix(data, []byte("six")) {
+			return 3, []byte("6"), nil
+		} else if bytes.HasPrefix(data, []byte("seven")) {
+			return 5, []byte("7"), nil
+		} else if bytes.HasPrefix(data, []byte("eight")) {
+			return 5, []byte("8"), nil
+		} else if bytes.HasPrefix(data, []byte("nine")) {
+			return 4, []byte("9"), nil
+		} else if bytes.HasPrefix(data, []byte("zero")) {
+			return 4, []byte("0"), nil
+		}
+	}
+
+	return 1, data[:1], nil
+}
+
+// EnhancedParseInput takes into account that the input may contain
+// words that represent numbers into the parsing.
+//
+// It returns a slice of ints.
+func EnhancedParseInput(input string) []int {
+	var sb strings.Builder
+	var scanner = bufio.NewScanner(strings.NewReader(input))
+	scanner.Split(tokenizer)
+
+	for scanner.Scan() {
+		sb.WriteString(scanner.Text())
+	}
+
+	return ParseInput(sb.String())
+}
+
+// ParseNumber takes a string, which should be a number, and returns
+// the first and last digit of the number.
+//
+// If the number is only one digit, it will be repeated twice.
+//
+// This function panics if the input is not a number.
 func ParseNumber(input string) int {
 	if len(input) == 1 {
-		input = strings.Repeat(input, 2)
+		// input = strings.Repeat(input, 2)
 	} else if len(input) > 2 {
 		input = string(input[0]) + string(input[len(input)-1])
 	}
 
-	parsedNumber, _ := strconv.Atoi(input)
+	parsedNumber, err := strconv.Atoi(input)
+
+	if err != nil {
+		panic(err)
+	}
+
 	return parsedNumber
 }
 
+// Solution to part 1 of day 1 of the Advent of Code 2023.
 func part1() {
 	numbers := ParseInput(data)
 	sum := 0
@@ -47,6 +132,18 @@ func part1() {
 	fmt.Printf("Part 1: %d\n", sum)
 }
 
+func part2() {
+	numbers := EnhancedParseInput(data)
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	fmt.Printf("Part 2: %d\n", sum)
+}
+
 func main() {
 	part1()
+	part2()
 }
--- 2023/day1/main_test.go
+++ 2023/day1/main_test.go
@@ -9,6 +9,9 @@ import (
 //go:embed data/2023_1_P1_example.txt
 var example1 string
 
+//go:embed data/2023_1_P2_example.txt
+var example2 string
+
 func TestParseInput(t *testing.T) {
 	t.Run("Gets the correct output", func(t *testing.T) {
 		got := ParseInput(example1)
@@ -16,6 +19,184 @@ func TestParseInput(t *testing.T) {
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("ParseInput(example) = %#v; want %#v", got, want)
+		}
+	})
+
+	t.Run("Handles empty input", func(t *testing.T) {
+		got := ParseInput("")
+		want := []int{}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("ParseInput(\"\") = %#v; want %#v", got, want)
+		}
+	})
+
+	t.Run("Handles empty lines", func(t *testing.T) {
+		got := ParseInput("\n\n")
+		want := []int{}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("ParseInput(\"\\n\\n\") = %#v; want %#v", got, want)
+		}
+	})
+}
+
+func TestEnhancedParseInput(t *testing.T) {
+	t.Run("Gets the correct output (example1)", func(t *testing.T) {
+		got := EnhancedParseInput(example1)
+		want := []int{12, 38, 15, 77}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(example1) = %#v; want %#v", got, want)
+		}
+	})
+
+	t.Run("Gets the correct output (example2)", func(t *testing.T) {
+		got := EnhancedParseInput(example2)
+		want := []int{29, 83, 13, 24, 42, 14, 76}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(example2) = %#v; want %#v", got, want)
+		}
+	})
+
+	t.Run("Handles one", func(t *testing.T) {
+		input := "one"
+		got := EnhancedParseInput(input)
+		want := []int{11}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles two", func(t *testing.T) {
+		input := "two"
+		got := EnhancedParseInput(input)
+		want := []int{22}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles three", func(t *testing.T) {
+		input := "three"
+		got := EnhancedParseInput(input)
+		want := []int{33}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles four", func(t *testing.T) {
+		input := "four"
+		got := EnhancedParseInput(input)
+		want := []int{44}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles five", func(t *testing.T) {
+		input := "five"
+		got := EnhancedParseInput(input)
+		want := []int{55}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles six", func(t *testing.T) {
+		input := "six"
+		got := EnhancedParseInput(input)
+		want := []int{66}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles seven", func(t *testing.T) {
+		input := "seven"
+		got := EnhancedParseInput(input)
+		want := []int{77}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles eight", func(t *testing.T) {
+		input := "eight"
+		got := EnhancedParseInput(input)
+		want := []int{88}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles nine", func(t *testing.T) {
+		input := "nine"
+		got := EnhancedParseInput(input)
+		want := []int{99}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles zero", func(t *testing.T) {
+		input := "zero"
+		got := EnhancedParseInput(input)
+		want := []int{00}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles mixed inputs", func(t *testing.T) {
+		input := "one3"
+		got := EnhancedParseInput(input)
+		want := []int{13}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Skips other characters", func(t *testing.T) {
+		input := "one$3"
+		got := EnhancedParseInput(input)
+		want := []int{13}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles tricky inputs", func(t *testing.T) {
+		input := "eightwo"
+		got := EnhancedParseInput(input)
+		want := []int{88}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
+		}
+	})
+
+	t.Run("Handles tricky inputs (v2)", func(t *testing.T) {
+		input := "threeight"
+		got := EnhancedParseInput(input)
+		want := []int{33}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("EnhancedParseInput(%#v) = %#v; want %#v", input, got, want)
 		}
 	})
 }
