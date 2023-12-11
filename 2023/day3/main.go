e3a8793f28d6c3b683c2c76ef747841bb557454d
Pedro Piquero <gowikel@gmail.com>
2023-12-11 15:27:05 +0000

    Add day 3 solution

---
+++ 2023/day3/main.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"regexp"
+	"runtime"
+	"strings"
+)
+
+//go:embed data/2023_03.txt
+var data string
+
+func GetPoints(input string) [][2]int {
+	result := make([][2]int, 0)
+
+	for i, line := range strings.Split(input, "\n") {
+		for j, char := range line {
+			if (char >= '0' && char <= '9') || char == '.' {
+				continue
+			}
+
+			point := [2]int{i, j}
+			result = append(result, point)
+		}
+	}
+	return result
+}
+
+func GetRanges(input string, points [][2]int) [][3]int {
+	result := make([][3]int, 0, len(points)*3)
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
+	for _, point := range points {
+		x := point[0]
+		y := point[1]
+
+		leftTarget := y - 1
+		rightTarget := y + 2 // [s:e] will not cover e
+
+		if leftTarget < 0 {
+			leftTarget = 0
+		}
+
+		if rightTarget >= len(lines[x]) {
+			rightTarget = len(lines[x]) - 1
+		}
+
+		if x > 0 {
+			result = append(
+				result,
+				[3]int{x - 1, leftTarget, rightTarget},
+			)
+		}
+
+		result = append(result, [3]int{x, leftTarget, rightTarget})
+
+		if x < len(lines)-1 {
+			result = append(
+				result,
+				[3]int{x + 1, leftTarget, rightTarget},
+			)
+		}
+	}
+
+	return result
+}
+
+func LocateNumbers(input string, ranges [][3]int) [][3]int {
+	number := regexp.MustCompile(`\d+`)
+
+	set := make(map[[3]int]bool)
+
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
+	// Calculate ranges
+	for _, r := range ranges {
+		lineIndex := r[0]
+		line := lines[lineIndex]
+
+		start := r[1]
+		end := r[2]
+
+		matches := number.FindAllStringIndex(line[start:end], -1)
+
+		if len(matches) > 0 {
+			for _, match := range matches {
+				startTarget := match[0]
+				endTarget := match[1]
+
+				if start > 0 {
+					startTarget += start
+					endTarget += start
+				}
+
+				// Expand to the left
+				for startTarget != 0 {
+					nextElement := line[startTarget-1]
+
+					if !(nextElement >= '0' && nextElement <= '9') {
+						break
+					}
+
+					startTarget -= 1
+				}
+
+				// Expand to the right
+				for endTarget < len(line) {
+					nextElement := line[endTarget]
+
+					if !(nextElement >= '0' && nextElement <= '9') {
+						break
+					}
+
+					endTarget += 1
+				}
+
+				// Store the range in a set to avoid repetitions
+				set[[3]int{lineIndex, startTarget, endTarget}] = true
+			}
+		}
+	}
+
+	// Convert the set into a [][3]int
+	result := make([][3]int, 0, len(set))
+	for r := range set {
+		result = append(result, r)
+	}
+
+	return result
+}
+
+func Part1(data string) int {
+	points := GetPoints(data)
+	ranges := GetRanges(data, points)
+	numberRanges := LocateNumbers(data, ranges)
+	lines := strings.Split(data, "\n")
+
+	var result int
+
+	for _, numberRange := range numberRanges {
+		lineIdx := numberRange[0]
+		start := numberRange[1]
+		end := numberRange[2]
+
+		number := lines[lineIdx][start:end]
+		var parsedInt int
+
+		_, err := fmt.Sscanf(number, "%d", &parsedInt)
+
+		if err != nil {
+			panic(err)
+		}
+
+		result += parsedInt
+	}
+
+	return result
+}
+
+func GetGears(input string) [][2]int {
+	lines := strings.Split(input, "\n")
+	result := make([][2]int, 0)
+
+	for i, line := range lines {
+		for j, symbol := range line {
+			if symbol == '*' {
+				result = append(result, [2]int{i, j})
+			}
+		}
+	}
+
+	return result
+}
+
+func Part2(data string) int {
+	lines := strings.Split(data, "\n")
+	potentialGears := GetGears(data)
+
+	var result int
+
+	for _, potentialGear := range potentialGears {
+		ranges := GetRanges(data, [][2]int{potentialGear})
+		numberRanges := LocateNumbers(data, ranges)
+
+		// To be a gear, you must have two different numbers around
+		if len(numberRanges) == 2 {
+			var sb strings.Builder
+			var g1 int
+			var g2 int
+
+			for _, numberRange := range numberRanges {
+				lineIdx := numberRange[0]
+				start := numberRange[1]
+				end := numberRange[2]
+
+				n := lines[lineIdx][start:end]
+
+				sb.WriteString(n)
+				sb.WriteRune(' ')
+			}
+
+			numbers := sb.String()
+			_, err := fmt.Sscanf(numbers, "%d %d ", &g1, &g2)
+
+			if err != nil {
+				panic(err)
+			}
+
+			result += g1 * g2
+		}
+	}
+
+	return result
+}
+
+func main() {
+	p1Sol := Part1(data)
+	p2Sol := Part2(data)
+
+	fmt.Printf("Part 1: %d\n", p1Sol)
+	fmt.Printf("Part 2: %d\n", p2Sol)
+	fmt.Printf("%v\n", runtime.Version())
+}
---
+++ 2023/day3/main_test.go
@@ -0,0 +1,434 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+)
+
+//go:embed data/2023_03_E1.txt
+var example1 string
+
+type GetPointsTestCase struct {
+	description string
+	input       string
+	expected    [][2]int
+}
+
+type GetRangesTestCase struct {
+	description string
+	input       string
+	points      [][2]int
+	expected    [][3]int
+}
+
+type LocateNumbersTestCase struct {
+	description string
+	input       string
+	ranges      [][3]int
+	expected    [][3]int
+}
+
+type GetGearsTestCase struct {
+	description string
+	input       string
+	expected    [][2]int
+}
+
+//	0123456789
+//
+// 0 123......9
+// 1 10*.......
+// 2 11.456.78.
+// 3 ..........
+// 4 ...#......
+// 5 ........09
+// 6 666....33$
+const input = "123......9\n" +
+	"10*.......\n" +
+	"11.456.78.\n" +
+	"..........\n" +
+	"...#......\n" +
+	"........09\n" +
+	"666....33$\n"
+
+func compareSets3Int(a, b [][3]int) bool {
+	setA := make(map[[3]int]bool)
+	setB := make(map[[3]int]bool)
+
+	for _, v := range a {
+		setA[v] = true
+	}
+
+	for _, v := range b {
+		setB[v] = true
+	}
+
+	if len(setA) != len(setB) {
+		return false
+	}
+
+	for v := range setA {
+		if !setB[v] {
+			return false
+		}
+	}
+
+	for v := range setB {
+		if !setA[v] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func compareSets2Int(a, b [][2]int) bool {
+	setA := make(map[[2]int]bool)
+	setB := make(map[[2]int]bool)
+
+	for _, v := range a {
+		setA[v] = true
+	}
+
+	for _, v := range b {
+		setB[v] = true
+	}
+
+	if len(setA) != len(setB) {
+		return false
+	}
+
+	for v := range setA {
+		if !setB[v] {
+			return false
+		}
+	}
+
+	for v := range setB {
+		if !setA[v] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestGetPoints(t *testing.T) {
+	testCases := []GetPointsTestCase{
+		{
+			description: "Symbols are located in a string",
+			input:       "...123...+\n...*45....\n67...&....",
+			expected: [][2]int{
+				{0, 9},
+				{1, 3},
+				{2, 5},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			got := GetPoints(testCase.input)
+			want := testCase.expected
+
+			if !compareSets2Int(got, want) {
+				t.Errorf(
+					"\n\n%s\n\nInput: %v\nGot: %v\nWant: %v\n\n",
+					testCase.description,
+					testCase.input,
+					got,
+					testCase.expected,
+				)
+			}
+		})
+	}
+}
+
+//   0123456789
+// 0 123......9
+// 1 10*.......
+// 2 11.456.78.
+// 3 ..........
+// 4 ...#......
+// 5 ........09
+// 6 666....33$
+
+func TestGetRanges(t *testing.T) {
+	testCases := []GetRangesTestCase{
+		{
+			description: "No points, no ranges",
+			input:       input,
+			points:      [][2]int{},
+			expected:    [][3]int{},
+		},
+		{
+			description: "A point on the first line will trigger " +
+				"a range in that line and the next",
+			input: input,
+			points: [][2]int{
+				{0, 1},
+				{0, 5},
+			},
+			expected: [][3]int{
+				{0, 0, 3},
+				{1, 0, 3},
+				{0, 4, 7},
+				{1, 4, 7},
+			},
+		},
+		{
+			description: "A point on the last line will trigger " +
+				"a range in that line and the previous one",
+			input: input,
+			points: [][2]int{
+				{6, 1},
+			},
+			expected: [][3]int{
+				{5, 0, 3},
+				{6, 0, 3},
+			},
+		},
+		{
+			description: "A point not close to any boundary will trigger " +
+				"a range in the previous line, that line and the next one",
+			input: input,
+			points: [][2]int{
+				{2, 1},
+			},
+			expected: [][3]int{
+				{1, 0, 3},
+				{2, 0, 3},
+				{3, 0, 3},
+			},
+		},
+		{
+			description: "Points at the beginning will have shorter ranges",
+			input:       input,
+			points: [][2]int{
+				{0, 0},
+				{3, 0},
+			},
+			expected: [][3]int{
+				{0, 0, 2},
+				{1, 0, 2},
+				{2, 0, 2},
+				{3, 0, 2},
+				{4, 0, 2},
+			},
+		},
+		{
+			description: "Points at the end of the line will have shorter ranges",
+			input:       input,
+			points: [][2]int{
+				{0, 9},
+				{3, 9},
+				{6, 9},
+			},
+			expected: [][3]int{
+				{0, 8, 9},
+				{1, 8, 9},
+				{2, 8, 9},
+				{3, 8, 9},
+				{4, 8, 9},
+				{5, 8, 9},
+				{6, 8, 9},
+			},
+		},
+		{
+			description: "Close points will cause overlaping ranges",
+			input:       input,
+			points: [][2]int{
+				{1, 1},
+				{1, 2},
+				{2, 1},
+			},
+			expected: [][3]int{
+				{0, 0, 3},
+				{1, 0, 3},
+				{2, 0, 3},
+				{0, 1, 4},
+				{1, 1, 4},
+				{2, 1, 4},
+				{1, 0, 3},
+				{2, 0, 3},
+				{3, 0, 3},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			got := GetRanges(testCase.input, testCase.points)
+			want := testCase.expected
+
+			if !compareSets3Int(got, want) {
+				t.Errorf(
+					"\n\n%s\n\nInput:\n%v\nPoints: %v\nGot: %v\nWant: %v\n\n",
+					testCase.description,
+					testCase.input,
+					testCase.points,
+					got,
+					want,
+				)
+			}
+		})
+	}
+}
+
+//   0123456789
+// 0 123......9
+// 1 10*.......
+// 2 11.456.78.
+// 3 ..........
+// 4 ...#......
+// 5 ........09
+// 6 666....33$
+
+func TestLocateNumbers(t *testing.T) {
+	testCases := []LocateNumbersTestCase{
+		{
+			description: "A range over a number will just return it",
+			input:       input,
+			ranges: [][3]int{
+				{0, 0, 2},
+				{2, 7, 8},
+			},
+			expected: [][3]int{
+				{0, 0, 3},
+				{2, 7, 9},
+			},
+		},
+		{
+			description: "A range partially over a number, will expand to the cover the number",
+			input:       input,
+			ranges: [][3]int{
+				{0, 0, 1},
+				{1, 1, 2},
+				{2, 2, 4},
+				{6, 2, 4},
+			},
+			expected: [][3]int{
+				{0, 0, 3},
+				{1, 0, 2},
+				{2, 3, 6},
+				{6, 0, 3},
+			},
+		},
+		{
+			description: "A range that spans multiple numbers, will return all of them",
+			input:       input,
+			ranges: [][3]int{
+				{2, 0, 6},
+			},
+			expected: [][3]int{
+				{2, 0, 2},
+				{2, 3, 6},
+			},
+		},
+		{
+			description: "An empty range will not return anything",
+			input:       input,
+			ranges: [][3]int{
+				{0, 0, 0},
+			},
+			expected: [][3]int{},
+		},
+		{
+			description: "Multiple ranges over the same number do not repeat the number",
+			input:       input,
+			ranges: [][3]int{
+				{0, 0, 1},
+				{0, 1, 2},
+			},
+			expected: [][3]int{
+				{0, 0, 3},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			got := LocateNumbers(testCase.input, testCase.ranges)
+			want := testCase.expected
+
+			if !compareSets3Int(got, want) {
+				t.Errorf(
+					"\n\n%s\n\nInput:\n%v\nRanges: %v\nGot: %v\nWant: %v\n\n",
+					testCase.description,
+					testCase.input,
+					testCase.ranges,
+					got,
+					want,
+				)
+			}
+		})
+	}
+}
+
+func TestPart1(t *testing.T) {
+	got := Part1(example1)
+	want := 4361
+
+	if got != want {
+		t.Errorf(
+			"\n\nPart 1\n\nInput:\n%v\nGot: %v\nWant: %v\n\n",
+			example1,
+			got,
+			want,
+		)
+	}
+}
+
+func TestGetGears(t *testing.T) {
+	testCases := []GetGearsTestCase{
+		{
+			description: "Empty input, no points",
+			input:       "",
+			expected:    [][2]int{},
+		},
+		{
+			description: "* positions are returned",
+			input: ".*%$.*.2.*\n" +
+				"..........\n" +
+				"...*&*....",
+			expected: [][2]int{
+				{0, 1},
+				{0, 5},
+				{0, 9},
+				{2, 3},
+				{2, 5},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			got := GetGears(testCase.input)
+			want := testCase.expected
+
+			if !compareSets2Int(got, want) {
+				t.Errorf(
+					"\n\n%s\n\nInput:\n%s\nGot: %v\nWant: %v\n\n",
+					testCase.description,
+					testCase.input,
+					got,
+					want,
+				)
+			}
+		})
+	}
+}
+
+func TestPar2(t *testing.T) {
+	got := Part2(example1)
+	want := 467835
+
+	if got != want {
+		t.Errorf(
+			"\n\nPart 1\n\nInput:\n%v\nGot: %v\nWant: %v\n\n",
+			example1,
+			got,
+			want,
+		)
+	}
+}
