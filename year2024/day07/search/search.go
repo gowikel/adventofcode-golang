c304aec39023421b35eec433393d7957e12a5cfb
Pedro Piquero <4339072+gowikel@users.noreply.github.com>
2025-12-17 18:55:44 +0000

    Extract search algorithm

--- year2024/day07/search/search.go
+++ year2024/day07/search/search.go
@@ -7,6 +7,21 @@ import (
 )
 
 func SearchPart1(eq parser.Equation) bool {
+	generator := func(lst []int, value int) []int {
+		newValues := make([]int, 2*len(lst))
+
+		for i, bufferedValue := range lst {
+			newValues[2*i] = bufferedValue + value
+			newValues[2*i+1] = bufferedValue * value
+		}
+
+		return newValues
+	}
+
+	return searchAlgorithm(eq, generator)
+}
+
+func searchAlgorithm(eq parser.Equation, generator func([]int, int) []int) bool {
 	if len(eq.Operands) == 0 && eq.Result != 0 {
 		return false
 	}
@@ -27,12 +42,7 @@ func SearchPart1(eq parser.Equation) bool {
 	buffer[0] = eq.Operands[0]
 
 	for _, operand := range eq.Operands[1:] {
-		newValues := make([]int, 2*len(buffer))
-
-		for i, value := range buffer {
-			newValues[2*i] = value + operand
-			newValues[2*i+1] = value * operand
-		}
+		newValues := generator(buffer, operand)
 
 		newValues = slices.DeleteFunc(newValues, func(value int) bool {
 			return value > eq.Result
