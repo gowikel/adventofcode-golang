64b1458e1fd79aa7bfdde9ffcee053a1c32246d7
Pedro Piquero <4339072+gowikel@users.noreply.github.com>
2026-02-16 00:52:35 +0000

    Update test package and import for day03 test

--- year2023/day03/exercise_test.go
+++ year2023/day03/exercise_test.go
@@ -1,16 +1,17 @@
-package day03
+package day03_test
 
 import (
 	_ "embed"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.eryndalor.dev/adventofcode-golang/year2023/day03"
 )
 
 //go:embed testdata/2023_03_example.txt
 var EXAMPLE string
 
-var e Exercise
+var e day03.Exercise
 
 func TestDay03Part1(t *testing.T) {
 	t.Skip("It needs review")
